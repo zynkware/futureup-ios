ce6e951aa62d414706548064677d84f182ddfc8b
Alex Bardea <alex.bardea@zynksoftware.com>
2022-03-22 11:21:03 +0200

    Initial Commit

---
+++ future-up-ios/future_up_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  future_up_iosApp.swift
+//  future-up-ios
+//
+//  Created by Alex Bardea on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct future_up_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ future-up-iosTests/future_up_iosTests.swift
@@ -0,0 +1,33 @@
+//
+//  future_up_iosTests.swift
+//  future-up-iosTests
+//
+//  Created by Alex Bardea on 22.03.2022.
+//
+
+import XCTest
+@testable import future_up_ios
+
+class future_up_iosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
